2c1ecc58115e1cb9426d4fbe8ec89f2d67c8476e
Ghislee <ghislaines.amoussou@gmail.com>
2020-09-11 18:23:18 +0100

    final commit

--- sw.js
+++ sw.js
@@ -1,3 +1,15 @@
 self.addEventListener("install", evt =>{
-    console.log("Install!");
-})
+    evt.waitUntil(
+        caches.open("static").then(cache => {
+            return cache.addAll(["./", "style.css", "images/photo-1578086591939-f3e05b7dbfdd.jpg"]);
+        })
+    );
+});
+
+self.addEventListener("fetch", evt =>{
+    evt.respondWith(
+        caches.match(evt.request).then(response => {
+            return response || fetch(evt.request);
+        })
+    );
+}); 
